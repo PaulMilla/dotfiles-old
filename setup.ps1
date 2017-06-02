1b39e3bf7700ded4ed43d12fb67d0321ad8e638f
Paul Milla <paul.milla@mattersight.com>
2017-06-02 16:31:44 -0500

    Add visual studio code dotfiles

--- setup.ps1
+++ setup.ps1
@@ -39,14 +39,26 @@ $file = ".vsvimrc" # Is this really needed?
 Create-HardLink -Path "$HOME\$file" -Value "$PSScriptRoot\$file"
 
 $file = "profile.ps1"
-#New-Item -ItemType HardLink -Path "$HOME\Documents\WindowsPowerShell\$file" -Value "$PSScriptRoot\$file"
+#Create-HardLink -Path "$HOME\Documents\WindowsPowerShell\$file" -Value "$PSScriptRoot\$file"
 
+# Visual Studio Code Settings
+$file = "settings.json"
+Create-HardLink -Path "$env:APPDATA\Code\User\$file" -Value "$PSScriptRoot\VisualStudioCode\$file"
+
+# Visual Studio Code Key
+$file = "keybindings.json"
+Create-HardLink -Path "$env:APPDATA\Code\User\$file" -Value "$PSScriptRoot\VisualStudioCode\$file"
+
+
+# TODO: Install NeoVim
 
 # Install the vim plugin-manager that we will use in our vim configs
 if (-Not (Test-Path $HOME/.vim/bundle/Vundle.vim)) {
   iex "git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim"
 }
 
+# TOOD: Install Fira Code font - https://github.com/tonsky/FiraCode
+# TODO: First find a way to find out if powerline fonts are already installed
 # Ask whether or not to install powerline fonts
 $install_fonts = Read-Host "Install Powerline Fonts (https://github.com/powerline/fonts)? [Y/n]"
 if ($install_fonts -EQ "Y") {
